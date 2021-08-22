c4a817fd47c81e2dea6c258281a9e4118f6f3a31
Csaba Kiss <csaba.kiss@tipico.com>
2021-08-22 17:47:52 +0200

    modules and changes #99

--- middleware.go
+++ middleware.go
@@ -85,7 +85,7 @@ func process(buf []byte) {
 
 	switch payloadType {
 	case '1':
-		Debug("- - - - - - - - - - - ", reqID)
+		// Debug("- - - - - - - - - - - ", reqID)
 		for key, ele := range hs {
 			if key == "Cookie" {
 				resp := get_session_id_from_cookie(ele)
@@ -127,22 +127,22 @@ func process(buf []byte) {
 	case '3':
 		// Debug("x x x x x x x x x x x ", reqID)
 		// Debug("= = = = = = = = = = = ", ele)
-		if _, ok := sessionIDs[reqID]; ok {
-			for key, _ := range hs {
-				if key == "Set-Cookie" {
-					// resp := get_session_id(ele)
-					// Debug("--- :REQ ID ", sessionIDs)
-					// ridval, exist := sessionIDs[reqID]
-					// if exist {
-					Debug("x x x x x x x x x x x ", sessionIDs[reqID])
-					// Debug("= = = = = = = = = = = ", ele)
-					// Debug("--- GETTING NEW COOKIE: ", ridval)
-					// ridval.new = ele
-					// }
-				}
+		// if _, ok := sessionIDs[reqID]; ok {
+		for key, _ := range hs {
+			if key == "Set-Cookie" {
+				// resp := get_session_id(ele)
+				// Debug("--- :REQ ID ", sessionIDs)
+				// ridval, exist := sessionIDs[reqID]
+				// if exist {
+				Debug("x x x x x x x x x x x ", sessionIDs[reqID])
+				// Debug("= = = = = = = = = = = ", ele)
+				// Debug("--- GETTING NEW COOKIE: ", ridval)
+				// ridval.new = ele
+				// }
 			}
-			// Debug(":: Status: ", string(proto.Status(payload)))
 		}
+		// Debug(":: Status: ", string(proto.Status(payload)))
+		// }
 	}
 }
 
